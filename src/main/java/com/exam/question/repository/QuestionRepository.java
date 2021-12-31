6fcf60c8ed9f8a9b8995f9173f329dddd19e209f
Ankit Kumar <ankitdayal1992@gmail.com>
2022-01-01 00:38:05 +0530

    Get All Questions API bug fix

--- src/main/java/com/exam/question/repository/QuestionRepository.java
+++ src/main/java/com/exam/question/repository/QuestionRepository.java
@@ -129,7 +129,7 @@ public class QuestionRepository extends BaseRepository {
         try{
             final StringBuilder sql = new StringBuilder("SELECT q.* , ");
             sql.append("qc.id as qc_id, ");
-            sql.append("qc.content as qc_desc, ");
+            sql.append("qc.description as qc_desc, ");
             sql.append("qc.enabled as qc_enabled, ");
             sql.append("qc.correct as qc_correct ");
             sql.append("FROM questions q ");
--- src/main/java/com/exam/question/repository/rowmappers/QuestionDataItemRowMapper.java
+++ src/main/java/com/exam/question/repository/rowmappers/QuestionDataItemRowMapper.java
@@ -14,7 +14,7 @@ public class QuestionDataItemRowMapper implements RowMapper {
     public Object mapRow(ResultSet rs, int i) throws SQLException {
         QuestionDataItem questionDataItem = new QuestionDataItem();
         questionDataItem.setId(rs.getInt("id"));
-        questionDataItem.setContent(rs.getString("description"));
+        questionDataItem.setContent(rs.getString("content"));
         questionDataItem.setEnabled(rs.getBoolean("enabled"));
         questionDataItem.setProficiencyId(rs.getInt("proficiency_id"));
         questionDataItem.setTopicId(rs.getInt("topic_id"));
