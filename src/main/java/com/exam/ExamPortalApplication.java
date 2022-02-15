abcef490820a696bbe99f4e52a1e6754e671a879
Ankit Kumar <ankitdayal1992@gmail.com>
2022-02-15 22:48:33 +0530

    Code Cleanup | Removing unused code

--- src/main/java/com/exam/ExamPortalApplication.java
+++ src/main/java/com/exam/ExamPortalApplication.java
@@ -1,42 +1,12 @@
 package com.exam;
 
-import com.exam.user.model.Role;
-import com.exam.user.model.User;
-import com.exam.user.repository.rowmappers.UserRowMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.List;
-
-import static com.exam.datafactory.Factory.*;
 
 @SpringBootApplication
-public class ExamPortalApplication implements CommandLineRunner {
-
-	@Autowired
-	JdbcTemplate jdbcTemplate;
+public class ExamPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamPortalApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Starting Exam Portal Server....");
-		System.out.println("Populating default master data....");
-
-		List<Role> list = jdbcTemplate.query("Select id, role_name from roles",
-				(rs, rowNum) -> new Role( rs.getInt("id"),rs.getString("role_name"))
-				);
-
-		getAllRoles(list);
-
-		List<User> users = jdbcTemplate.query("Select * from users", new UserRowMapper());
-
-		getUsers(users);
-
 	}
 }
